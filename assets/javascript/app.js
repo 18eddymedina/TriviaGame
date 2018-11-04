7b976256930ccedcdbe525f7f16599202ff14900
18eddymedina <18emedina@gmail.com>
2018-11-03 20:33:44 -0500

    second commit

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -13,7 +13,7 @@ var trivia = {
 
       },
       {
-        question1:"What did the dog say backwards when mabel had too much Smile dip?",
+        question1:"What did the dog say backwards when Mabel had too much Smile dip?",
         answers: ["Don't trust Grunkle","Leave the Falls","Beware Him","You Will Perish"],    
         correct: 0,
 
@@ -36,19 +36,61 @@ var trivia = {
         correct: 0,
         
       },
+      {
+        question1: "What is the name of Mabel's pig",
+        answers: ["Piggly","Waddles","Mister Wiggles","Porkers"],    
+        correct: 1,
+        
+      },
+      {
+        question1: "What was on Dipper's original cap?",
+        answers: ["A skull","A Tree","A star","A triangle"],    
+        correct: 2,
+        
+      },
+      {
+        question1: "How much does Mabel's pig weigh?",
+        answers: ["8 Pounds","10 Pounds","12 Pounds","15 Pounds"],    
+        correct: 3,
+        
+      },
+      {
+        question1: "What is Mabel's go to weapon?",
+        answers: ["Bat","Grappling Hook","Leafblower","Sword"],    
+        correct: 1,
+        
+      },
+      {
+        question1: "Who does Mabel end up kissing?",
+        answers: ["Mermando","Gabe","Several Timez","Norman"],    
+        correct: 0,
+        
+      },
+      {
+        question1: "What aspect of Mabel changes in every episode?",
+        answers: ["Her hair","Her shoes","Her shirt","Her sweater"],    
+        correct: 3,
+        
+      },
+      {
+        question1: "Which of these is NOT a television show in the show",
+        answers: ["Duck-tective","Why You Ackin' SO Cray-Cray","Grampa the Kid","Lamby Lamb"],    
+        correct: 3,
+        
+      },
     ],
 
 
 
 
       startgif:"https://media.giphy.com/media/JsnbYw3mMgObe/giphy.gif",
-      correctgifs:["correct1.gif","correct2.gif","correct3.gif","correct4.gif","correct5.gif"],
-      incorrectgifs:["Incorrect1.gif","Incorrect2.gif","Incorrect3.gif","Incorrect4.gif","Incorrect5.gif","Incorrect6.gif","Incorrect7.gif","Incorrect8.gif"],
+      correctgifs:["assets/images/correct1.gif","assets/images/correct2.gif","assets/images/correct3.gif","assets/images/correct4.gif","assets/images/correct5.gif","assets/images/correct6.gif","assets/images/correct7.gif","assets/images/correct8.gif","assets/images/correct9.gif","assets/images/correct10.gif",],
+      incorrectgifs:["assets/images/incorrect1.gif","assets/images/incorrect2.gif","assets/images/incorrect3.gif","assets/images/incorrect4.gif","assets/images/incorrect5.gif","assets/images/incorrect6.gif","assets/images/incorrect7.gif","assets/images/incorrect8.gif","assets/images/incorrect9.gif","assets/images/incorrect10.gif","assets/images/incorrect11.gif"],
 total:[],
 gifs:[],
-num: Math.floor(Math.random()*6),
-corgifs: Math.floor(Math.random()*5),
-ingifs: Math.floor(Math.random()*8),
+num: Math.floor(Math.random()*13),
+corgifs: Math.floor(Math.random()*10),
+ingifs: Math.floor(Math.random()*11),
 numberCorrect:0,
 numberIncorrect:0,
 timeleft:10,
@@ -85,7 +127,7 @@ endGame: function(){
 },
 
 play: function(){
-    trivia.timeleft=15;
+    trivia.timeleft=10;
     trivia.top.style.display="block";
      trivia.answerBlock.style.display="block";
      trivia.revealCorrect.style.display="none";
@@ -95,15 +137,15 @@ play: function(){
      trivia.end.style.display="none";
      trivia.results.style.display="none";
 
-    if(trivia.total.length>4)
+    if(trivia.total.length>9)
     {
         trivia.endGame();
     }
     else{
-    trivia.num= Math.floor(Math.random()*6);
+    trivia.num= Math.floor(Math.random()*13);
     
     while(trivia.total.indexOf(trivia.num)>=0){
-        trivia.num= Math.floor(Math.random()*6);
+        trivia.num= Math.floor(Math.random()*13);
     }
 
         clearInterval(x);
@@ -131,7 +173,7 @@ trivia.answerBlock.style.display="none";
 //trivia.revealIncorrect.style.display="block";
 trivia.show.style.display="block";
 while(trivia.gifs.indexOf(trivia.ingifs)>=0){
-    console.log("boo");
+  //  console.log("boo");
 
     trivia.ingifs= Math.floor(Math.random()*8);
 }
@@ -167,13 +209,13 @@ if(trivia.set[trivia.num].correct==number){
 
     while(trivia.gifs.indexOf(trivia.corgifs)>=0){
         console.log("yay");
-        trivia.corgifs= Math.floor(Math.random()*5);
+        trivia.corgifs= Math.floor(Math.random()*10);
     }
     trivia.gifs.push(trivia.corgifs);
 
-    document.getElementById("pic").src = trivia.correctgifs[trivia.corgifs];
+    document.getElementById("pic").src = trivia.correctgifs[0];
 
-
+    console.log(trivia.corgifs);
     document.getElementById("showanswer1").innerHTML = "The Correct Answer is: "+trivia.set[trivia.num].answers[trivia.set[trivia.num].correct];
     trivia.numberCorrect++;
    setTimeout(trivia.play,3000);
@@ -185,7 +227,7 @@ else{
     while(trivia.gifs.indexOf(trivia.ingifs)>=0){
         console.log("boo");
 
-        trivia.ingifs= Math.floor(Math.random()*8);
+        trivia.ingifs= Math.floor(Math.random()*11);
     }
 
     document.getElementById("pic").src = trivia.incorrectgifs[trivia.ingifs];
